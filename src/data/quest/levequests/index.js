b415fea8cfa735162ac34819bc0b3e674ae6f575
Triskele37 <triskele37@gmail.com>
2020-01-11 01:35:28 -0600

    added tradecraft leves

--- src/data/quest/levequests/index.js
+++ src/data/quest/levequests/index.js
@@ -1,15 +1,15 @@
 import { BattlecraftLeves } from "./battlecraft-leves";
-import { MiningLeves } from "./mining-leves";
-import { BotanyLeves } from "./botany-leves";
-import { FishingLeves } from "./fishing-leves";
-// import { CarpentryLeves } from "./carpentry-leves";
-// import { BlacksmithingLeves } from "./blacksmithing-leves";
-// import { ArmoringLeves } from "./armoring-leves";
-// import { GoldsmithingLeves } from "./goldsmithing-leves";
-// import { LeatherworkingLeves } from "./leatherworking-leves";
-// import { ClothcraftingLeves } from "./clothcrafting-leves";
-// import { AlchemyLeves } from "./alchemy-leves";
-// import { CookingLeves } from "./cooking-leves";
+import { MiningLeves } from "./fieldcraft/mining-leves";
+import { BotanyLeves } from "./fieldcraft/botany-leves";
+import { FishingLeves } from "./fieldcraft/fishing-leves";
+import { CarpentryLeves } from "./tradecraft/carpentry-leves";
+import { BlacksmithingLeves } from "./tradecraft/blacksmithing-leves";
+import { ArmoringLeves } from "./tradecraft/armoring-leves";
+import { GoldsmithingLeves } from "./tradecraft/goldsmithing-leves";
+import { LeatherworkingLeves } from "./tradecraft/leatherworking-leves";
+import { ClothcraftingLeves } from "./tradecraft/clothcrafting-leves";
+import { AlchemyLeves } from "./tradecraft/alchemy-leves";
+import { CookingLeves } from "./tradecraft/cooking-leves";
 
 export const Levequests = {
     name: "Levequests",
@@ -18,13 +18,13 @@ export const Levequests = {
         MiningLeves,
         BotanyLeves,
         FishingLeves,
-        // CarpentryLeves,
-        // BlacksmithingLeves,
-        // ArmoringLeves,
-        // GoldsmithingLeves,
-        // LeatherworkingLeves,
-        // ClothcraftingLeves,
-        // AlchemyLeves,
-        // CookingLeves,
+        CarpentryLeves,
+        BlacksmithingLeves,
+        ArmoringLeves,
+        GoldsmithingLeves,
+        LeatherworkingLeves,
+        ClothcraftingLeves,
+        AlchemyLeves,
+        CookingLeves,
     ]
 };
