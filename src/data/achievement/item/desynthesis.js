a425b2a54c7a671e0adf1a75253d31e46912870a
Danny <dlopes690@gmail.com>
2020-01-10 22:07:52 -0500

    Add Item Achievements

---
+++ src/data/achievement/item/desynthesis.js
@@ -0,0 +1,44 @@
+//https://xivapi.com/search?indexes=Achievement&string_column=AchievementCategory.Name_en&string=Desynthesis&columns=Name,Description,Points,Item.Name,Title.Name&page=1&limit=380
+module.exports = {
+    name: 'Desynthesis',
+    tableConfig: {
+        headers: ["Name", "Description", "Points", 'Reward'],
+        columnKeys: ['name', 'description', 'points', 'reward']
+    },
+    tasks: [{
+        description: "Successfully desynthesize 50 items.",
+        name: "Break It Down I",
+        points: 5,
+        reward: "-"
+    }, {
+        description: "Successfully desynthesize 200 items.",
+        name: "Break It Down II",
+        points: 5,
+        reward: "-"
+    }, {
+        description: "Successfully desynthesize 500 items.",
+        name: "Break It Down III",
+        points: 5,
+        reward: "-"
+    }, {
+        description: "Successfully desynthesize 1,000 items.",
+        name: "Break It Down IV",
+        points: 5,
+        reward: "-"
+    }, {
+        description: "Successfully desynthesize 2,000 items.",
+        name: "Break It Down V",
+        points: 10,
+        reward: "-"
+    }, {
+        description: "Successfully desynthesize 5,000 items.",
+        name: "You're Tearing Me Apart",
+        points: 10,
+        reward: "Title: The Disassembler"
+    }, {
+        description: "Successfully desynthesize 10,000 items.",
+        name: "You're Tearing Me Apart II",
+        points: 10,
+        reward: "-"
+    }]
+}
---
+++ src/data/achievement/item/materia.js
@@ -0,0 +1,104 @@
+//https://xivapi.com/search?indexes=Achievement&string_column=AchievementCategory.Name_en&string=Materia&columns=Name,Description,Points,Item.Name,Title.Name&page=1&limit=380
+module.exports = {
+    name: 'Materia',
+    tableConfig: {
+        headers: ["Name", "Description", "Points", 'Reward'],
+        columnKeys: ['name', 'description', 'points', 'reward']
+    },
+    tasks: [{
+        description: "Successfully affix 10 materia.",
+        name: "Getting Too Attached I",
+        points: 5,
+        reward: "-"
+    }, {
+        description: "Successfully affix 50 materia.",
+        name: "Getting Too Attached II",
+        points: 5,
+        reward: "-"
+    }, {
+        description: "Successfully affix 100 materia.",
+        name: "Getting Too Attached III",
+        points: 5,
+        reward: "-"
+    }, {
+        description: "Successfully affix 500 materia.",
+        name: "Getting Too Attached IV",
+        points: 5,
+        reward: "-"
+    }, {
+        description: "Successfully affix 1,000 materia.",
+        name: "Materia Hysteria",
+        points: 10,
+        reward: "Title: Carrier of the Cauldron"
+    }, {
+        description: "Successfully affix two materia to the same piece of gear.",
+        name: "Beginner's Luck",
+        points: 10,
+        reward: "-"
+    }, {
+        description: "Successfully affix three materia to the same piece of gear.",
+        name: "I Make My Own Luck",
+        points: 20,
+        reward: "-"
+    }, {
+        description: "Successfully affix four materia to the same piece of gear.",
+        name: "Luck's Got Nothing to Do with It",
+        points: 30,
+        reward: "-"
+    }, {
+        description: "Successfully affix five materia to the same piece of gear.",
+        reward: "Item: Gambler's Crown",
+        name: "I Got This!",
+        points: 40,
+    }, {
+        description: "Successfully convert 10 spiritbound items into materia.",
+        name: "Prepare to Be Assimilated I",
+        points: 5,
+        reward: "-"
+    }, {
+        description: "Successfully convert 50 spiritbound items into materia.",
+        name: "Prepare to Be Assimilated II",
+        points: 5,
+        reward: "-"
+    }, {
+        description: "Successfully convert 100 spiritbound items into materia.",
+        name: "Prepare to Be Assimilated III",
+        points: 5,
+        reward: "-"
+    }, {
+        description: "Successfully convert 500 spiritbound items into materia.",
+        name: "Prepare to Be Assimilated IV",
+        points: 5,
+        reward: "-"
+    }, {
+        description: "Successfully convert 1,000 spiritbound items into materia.",
+        name: "Living in a Materia World",
+        points: 10,
+        reward: "Title: The Assimilator"
+    }, {
+        description: "Obtain materia of a higher grade through transmutation.",
+        name: "I Got a Good Materia",
+        points: 10,
+        reward: "-"
+    }, {
+        description: "Successfully affix 2,500 materia.",
+        name: "Getting Too Attached V",
+        points: 10,
+        reward: "-"
+    }, {
+        description: "Successfully affix 5,000 materia.",
+        name: "Getting Too Attached VI",
+        points: 10,
+        reward: "-"
+    }, {
+        description: "Successfully convert 5,000 spiritbound items into materia.",
+        name: "Prepare to Be Assimilated V",
+        points: 10,
+        reward: "-"
+    }, {
+        description: "Successfully affix 10,000 materia.",
+        name: "Getting Too Attached VII",
+        points: 10,
+        reward: "-"
+    }]
+}
