e77edf2152c17b8c9f238873a7423f6c01dda04c
Triskele37 <triskele37@gmail.com>
2020-01-21 17:44:19 -0600

    added completion summaries

---
+++ src/renderer/section-summary/SectionSummary.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="summary-container">
+        <summary-line
+            v-for="subGroup in group.subGroups"
+            :key="subGroup.name"
+            :group="subGroup"
+        />
+    </div>
+</template>
+
+<script>
+    import SummaryLine from './SummaryLine';
+
+    export default {
+        name: 'section-summary',
+        components: {
+            'summary-line': SummaryLine,
+        },
+        props: {
+            group: Object
+        }
+    }
+</script>
+
+<style>
+    #summary-container {
+        height: calc(100% - 160px);
+        margin: 0 10px;
+        overflow-y: auto;
+    }
+</style>
