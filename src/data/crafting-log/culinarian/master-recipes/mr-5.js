37e54353374bebbc8eee7db64a7a01cae8ef457c
Danny Lopes <dlopes@palladianpartners.com>
2020-01-22 14:30:13 -0500

    Add Culincarian crafting log

--- src/data/crafting-log/culinarian/master-recipes/mr-5.js
+++ src/data/crafting-log/culinarian/master-recipes/mr-5.js
@@ -6,6 +6,65 @@ export const Culinarian_MasterRecipe_5 = {
   storageKey: "crafting-log.culinarian.master-recipes.master-5",
   columns: CraftingLogColumnConfig,
   tasks: [
-
-  ]
+    {
+      "level": "70★★",
+      "item": "Pork Kakuni",
+      "mat-1": "Koshu Pork",
+      "mat-2": "Oriental Soy Sauce",
+      "mat-3": "Cooking Sake",
+      "mat-4": "Jhammel Ginger",
+      "mat-5": "Fennel",
+      "mat-6": "Dusklight Aethersand",
+      "catalysts": "2 Fire Cluster, 2 Water Cluster",
+      "special": "Unlocked w/ Master Culinarian V"
+    },
+    {
+      "level": "70★★",
+      "item": "Chawan-mushi",
+      "mat-1": "Gagana Egg",
+      "mat-2": "Secret Recipe Broth",
+      "mat-3": "Ala Mhigan Salt Crystal",
+      "mat-4": "Shiitake Mushroom",
+      "mat-5": "Yanxian Parsley",
+      "mat-6": "Dusklight Aethersand",
+      "catalysts": "2 Fire Cluster, 2 Water Cluster",
+      "special": "Unlocked w/ Master Culinarian V"
+    },
+    {
+      "level": "70★★",
+      "item": "Sermon-worthy Meuniere",
+      "mat-1": "Lordly Salmon",
+      "mat-2": "Gyr Abanian Flour",
+      "mat-3": "Fermented Butter",
+      "mat-4": "Heavens Lemon",
+      "mat-5": "Young Cieldalaes Spinich",
+      "mat-6": "Crimson Pepper",
+      "catalysts": "2 Fire Cluster, 2 Water Cluster",
+      "special": "Unlocked w/ Master Culinarian V"
+    },
+    {
+      "level": "70★★",
+      "item": "Pork Stew",
+      "mat-1": "Koshu Pork",
+      "mat-2": "Fish Stock",
+      "mat-3": "Oriental Miso Paste",
+      "mat-4": "Daikon Radish",
+      "mat-5": "Gyr Abanian Carrot",
+      "mat-6": "Dusklight Aethersand",
+      "catalysts": "2 Fire Cluster, 2 Water Cluster",
+      "special": "Unlocked w/ Master Culinarian V"
+    },
+    {
+      "level": "70★★",
+      "item": "Steppe Tea",
+      "mat-1": "Tea Brick",
+      "mat-2": "Steppe Milk",
+      "mat-3": "Ala Mhigan Salt Crystal",
+      "mat-4": "Dark Chestnut",
+      "mat-5": "Dusklight Aethersand",
+      "mat-6": "X",
+      "catalysts": "2 Fire Cluster, 2 Water Cluster",
+      "special": "Ingredient (WVR), Unlocked w/ Master Culinarian V"
+    }
+   ]
 }
