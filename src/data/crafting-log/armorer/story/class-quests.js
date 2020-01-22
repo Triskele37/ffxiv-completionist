cb7c5c443f32d3784280c08d8255f04ae9a9b619
Danny Lopes <dlopes@palladianpartners.com>
2020-01-22 14:12:42 -0500

    Update Armorer crafting log

--- src/data/crafting-log/armorer/story/class-quests.js
+++ src/data/crafting-log/armorer/story/class-quests.js
@@ -6,6 +6,53 @@ export const Armorer_Story_ClassQuests = {
   storageKey: "crafting-log.armorer.story-recipes.class-quests",
   columns: CraftingLogColumnConfig,
   tasks: [
-
-  ]
+    {
+      "level": 63,
+      "item": "Paladin Mail",
+      "mat-1": "Plate Mail Materials",
+      "mat-2": "X",
+      "mat-3": "X",
+      "mat-4": "X",
+      "mat-5": "X",
+      "mat-6": "X",
+      "catalysts": "2 Ice Crystal, Earth Crystal",
+      "special": "X"
+    },
+    {
+      "level": 65,
+      "item": "Titanium Kote",
+      "mat-1": "Kote Materials",
+      "mat-2": "X",
+      "mat-3": "X",
+      "mat-4": "X",
+      "mat-5": "X",
+      "mat-6": "X",
+      "catalysts": "2 Ice Crystal, Earth Crystal",
+      "special": "X"
+    },
+    {
+      "level": 68,
+      "item": "Tournament Somen",
+      "mat-1": "Somen Materials",
+      "mat-2": "X",
+      "mat-3": "X",
+      "mat-4": "X",
+      "mat-5": "X",
+      "mat-6": "X",
+      "catalysts": "2 Ice Crystal, Earth Crystal",
+      "special": "X"
+    },
+    {
+      "level": 70,
+      "item": "Lominsan Hara-ate",
+      "mat-1": "Hara-ate Materials",
+      "mat-2": "X",
+      "mat-3": "X",
+      "mat-4": "X",
+      "mat-5": "X",
+      "mat-6": "X",
+      "catalysts": "2 Ice Crystal, Earth Crystal",
+      "special": "X"
+    }
+   ]
 }
