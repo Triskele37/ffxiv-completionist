3ba36bc8d27dc960450d3d7cc0d68c7796f7c568
Danny <dlopes690@gmail.com>
2020-01-22 19:28:03 -0500

    Add alchemist crafting logs

--- src/data/crafting-log/alchemist/level-based/level-1-5.js
+++ src/data/crafting-log/alchemist/level-based/level-1-5.js
@@ -7,5 +7,101 @@ export const AlchemistLevels_1_5 = {
   columns: CraftingLogColumnConfig,
   tasks: [
 
+    {
+      "level": "1",
+      "item": "Distilled Water",
+      "mat-1": "Muddy Water",
+      "mat-2": "X",
+      "mat-3": "X",
+      "mat-4": "X",
+      "mat-5": "X",
+      "mat-6": "X",
+      "catalysts": "Water Shard",
+      "special": "Buyable, Ingredient (ALC, CUL, GSM, LTW), Levequest"
+    },
+    {
+      "level": "1",
+      "item": "Quicksilver",
+      "mat-1": "2 Cinnabar",
+      "mat-2": "X",
+      "mat-3": "X",
+      "mat-4": "X",
+      "mat-5": "X",
+      "mat-6": "X",
+      "catalysts": "Water Shard",
+      "special": "Buyable, Ingredient (ALC, GSM), Levequest"
+    },
+    {
+      "level": "2",
+      "item": "Animal Glue",
+      "mat-1": "Animal Skin",
+      "mat-2": "2 Bone Chip",
+      "mat-3": "X",
+      "mat-4": "X",
+      "mat-5": "X",
+      "mat-6": "X",
+      "catalysts": "Water Shard",
+      "special": "Buyable, Ingredient (BSM, CRP, LTW), Levequest"
+    },
+    {
+      "level": "3",
+      "item": "Growth Formula Alpha",
+      "mat-1": "Shriekshroom",
+      "mat-2": "Quicksilver",
+      "mat-3": "Rock Salt",
+      "mat-4": "X",
+      "mat-5": "X",
+      "mat-6": "X",
+      "catalysts": "Water Shard",
+      "special": "Buyable, Ingredient (ALC, CRP), Levequest"
+    },
+    {
+      "level": "4",
+      "item": "Enchanted Copper Ink",
+      "mat-1": "2 Copper Sand",
+      "mat-2": "Beastkin Blood",
+      "mat-3": "X",
+      "mat-4": "X",
+      "mat-5": "X",
+      "mat-6": "X",
+      "catalysts": "Water Shard",
+      "special": "Ingredient (ALC)"
+    },
+    {
+      "level": "4",
+      "item": "Maple Wand",
+      "mat-1": "Maple Branch",
+      "mat-2": "Growth Formula Alpha",
+      "mat-3": "X",
+      "mat-4": "X",
+      "mat-5": "X",
+      "mat-6": "X",
+      "catalysts": "Water Shard, Lightning Shard",
+      "special": "Buyable, Ingredient (ALC), Levequest, Provisioning"
+    },
+    {
+      "level": "5",
+      "item": "Leather Grimoire",
+      "mat-1": "Enchanted Copper Ink",
+      "mat-2": "Leather",
+      "mat-3": "Maple Log",
+      "mat-4": "Hempen Yarn",
+      "mat-5": "X",
+      "mat-6": "X",
+      "catalysts": "Water Shard, Lightning Shard",
+      "special": "Buyable, Ingredient (ALC), Levequest, Provisioning"
+    },
+    {
+      "level": "5",
+      "item": "Antidote",
+      "mat-1": "Grass Viper",
+      "mat-2": "Rock Salt",
+      "mat-3": "X",
+      "mat-4": "X",
+      "mat-5": "X",
+      "mat-6": "X",
+      "catalysts": "Water Shard, Lightning Shard",
+      "special": "Buyable, Levequest, Provisioning"
+    },
   ]
 }
--- src/data/crafting-log/alchemist/others.js
+++ src/data/crafting-log/alchemist/others.js
@@ -6,6 +6,55 @@ export const Alchemist_Others = {
   storageKey: "crafting-log.alchemist.others-recipes",
   columns: CraftingLogColumnConfig,
   tasks: [
-
+    [
+      {
+        "level": "50★",
+        "item": "Wolf Grimoire of Casting",
+        "mat-1": "Enchanted Gold Ink",
+        "mat-2": "2 Electrum Ingot",
+        "mat-3": "Cobalt Ingot",
+        "mat-4": "Rosewood Log",
+        "mat-5": "Woolen Yarn",
+        "mat-6": "X",
+        "catalysts": "3 Water Crystal, 2 Lightning Crystal",
+        "special": "X"
+      },
+      {
+        "level": "50★",
+        "item": "Wolf Grimoire of Healing",
+        "mat-1": "Enchanted Gold Ink",
+        "mat-2": "2 Electrum Ingot",
+        "mat-3": "Cobalt Ingot",
+        "mat-4": "Rosewood Log",
+        "mat-5": "Woolen Yarn",
+        "mat-6": "X",
+        "catalysts": "3 Water Crystal, 2 Lightning Crystal",
+        "special": "X"
+      },
+      {
+        "level": "50★",
+        "item": "Icarus Wing",
+        "mat-1": "Eagle Feather",
+        "mat-2": "Gold Sand",
+        "mat-3": "Beeswax",
+        "mat-4": "X",
+        "mat-5": "X",
+        "mat-6": "X",
+        "catalysts": "3 Water Crystal, 2 Lightning Crystal",
+        "special": "X"
+      },
+      {
+        "level": "50★",
+        "item": "Regain Feather",
+        "mat-1": "Swan Feather",
+        "mat-2": "Growth Formula Delta",
+        "mat-3": "X",
+        "mat-4": "X",
+        "mat-5": "X",
+        "mat-6": "X",
+        "catalysts": "3 Water Crystal, 2 Lightning Crystal",
+        "special": "X"
+      }
+     ]
   ]
 }
