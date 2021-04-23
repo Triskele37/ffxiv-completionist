05537c25d7b7b5b6b371891a9536baf7f28afbf6
Triskele37 <triskele37@gmail.com>
2021-04-23 02:54:15 -0500

    sightseeing log spelling fix

--- src/store/electronStore/migration/5_45-5_5.js
+++ src/store/electronStore/migration/5_45-5_5.js
@@ -15,6 +15,7 @@ export const migrate_5_45_to_5_5 = () => {
 
     //--------------------------------- Spelling
     overall.change('duty.fates.gyr-abania.the-fringes', 'rasin-hell', 'raisin-hell');
+    overall.change('logs.sightseeing-log.sb', 'namal', 'namai');
 
     overall.write();
 };
