26f812e8f3cf2740737a4c5bc556755b05389a60
Triskele37 <triskele37@gmail.com>
2021-05-18 08:29:55 -0500

    clarified some saving instructions

--- src/renderer/pages/instructions/saving.vue
+++ src/renderer/pages/instructions/saving.vue
@@ -17,6 +17,28 @@
 
         <br/>
 
+        <h2>Files</h2>
+        <p>This app relies on 2+ files to handle your save data. A "config.json" file and your completion save/s (by default called "completion.json")</p>
+        <p>Initially they can be found in "C:/Users/YOURNAME/AppData/Roaming/ffxiv-completionist"</p>
+        <p>If you have moved your save and lost track of where it is, you can check "config.json" for the location</p>
+        <ul>
+            <li>Changing the "version" value in your completion save manually will cause issues when updating to new app versions</li>
+            <li>Entering invalid data into any fields in either file type will more than likely lead to breaking the app</li>
+        </ul>
+
+        <br/>
+
+        <h2>"Backup"</h2>
+        <p>I will always suggest "backing up" your completion save any time you update the app</p>
+        <p>The simplest way to manually do this is:</p>
+        <ol>
+            <li>Locate your completion save file</li>
+            <li><b>Copy</b> it to a safe location</li>
+            <li>Don't attempt to load it via the app</li>
+        </ol>
+
+        <br/>
+
         <h4>Final Note</h4>
         <p>Completion is saved as soon as items are marked</p>
         <p>Completion is migrated between versions of the app, sometimes things will need to be manually reviewed (these will be noted in the version history)</p>
