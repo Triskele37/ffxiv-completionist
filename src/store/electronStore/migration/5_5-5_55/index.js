ba2400c575498cdb14a0a14f5c4143892988e0a1
Triskele37 <triskele37@gmail.com>
2021-05-19 07:20:27 -0500

    finished common files for duty section

--- src/store/electronStore/migration/5_5-5_55/index.js
+++ src/store/electronStore/migration/5_5-5_55/index.js
@@ -6,6 +6,8 @@ import { migrate_5_55_barding } from "./barding";
 export const migrate_5_5_to_5_55 = () => {
     const overall = new ChangeStore('0.5.55', true);
 
+    overall.changeKey("duty.quests.other", "undefined", "quasi-quests");
+
     //TODO: Don't forget to uncomment
     // migrate_5_55_titles(overall);
     // migrate_5_55_barding(overall);
