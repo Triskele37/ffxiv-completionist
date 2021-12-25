1aa719e8a5edf8efcababeecbb3b24f51f5858ec
Triskele37 <triskele37@gmail.com>
2021-12-24 18:37:02 -0600

    applied rgba to root var, fixed table stripes

--- src/app/core/component/task-table/cell/actions/actions-cell.component.ts
+++ src/app/core/component/task-table/cell/actions/actions-cell.component.ts
@@ -24,21 +24,21 @@ export class ActionsCellComponent {
 
     gotoGamerEscape() {
         let name = this.task.name.replace(/ /g, '_');
-        name = gamerEscapeSafeMap(name);
+        name = this.gamerEscapeSafeMap(name);
 
         ElectronService.remote.shell.openExternal(`https://ffxiv.gamerescape.com/wiki/${name}`);
+    }
+
+    gamerEscapeSafeMap(name) {
+        const match = (n, id) => this.task.name === n && this.task.id === id;
+
+        if(match('Mending Fences', 143)) return `${name}_(Levequest)`;
+
+        return name;
     }
 
     gotoGarlandTools() {
         const name = this.task.name.replace(/ /g, '%20');
         ElectronService.remote.shell.openExternal(`https://www.garlandtools.org/db/#search/${name}`);
     }
-}
-
-function gamerEscapeSafeMap(name) {
-    const match = (n, id) => this.task.name === n && this.task.id === id;
-
-    if(match('Mending Fences', 143)) return `${name}_(Levequest)`;
-
-    return name;
 }
