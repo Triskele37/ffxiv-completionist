bfa9f928a85289e06e937880078b8c870590fbcd
Triskele37 <triskele37@gmail.com>
2022-04-02 06:25:43 -0500

    fixed broken chain links

--- src/app/view/random/random.component.ts
+++ src/app/view/random/random.component.ts
@@ -21,6 +21,41 @@ export class RandomComponent {
         if(lastRandom.length) this.randomTasks = lastRandom;
     }
 
+    //TODO: Create a developer settings menu and add this as an action
+    // this runs through every task changing its completion to ensure no
+    // links are broken (it takes nearly 10 minutes)
+    // first = true;
+    // test() {
+    //     this.dive(this.svcData.data, 0);
+    //     console.log('Done');
+    // }
+    // dive(group: DataGroup, depth: number) {
+    //     const tab = new Array(depth).fill(null).join('  ');
+    //     console.log(tab + group._key);
+    //     group.subGroups?.forEach((sg) => this.dive(sg, depth + 1));
+    //     group.tasks?.forEach((task) => {
+    //         try {
+    //             if(task.isNumericCompletion) {
+    //                 if(task.completionFlag !== task.defaultCompletion.toString()) {
+    //                     task.changeCompletion(task.defaultCompletion);
+    //                 }
+    //
+    //                 task.changeCompletion(task.maxValue.toString());
+    //                 task.changeCompletion(task.minValue.toString());
+    //             }
+    //             else {
+    //                 if(task.completionFlag !== 'N') task.changeCompletion('N');
+    //                 task.changeCompletion('Y', this.first);
+    //                 task.changeCompletion('N');
+    //             }
+    //         }
+    //         catch(e) {
+    //             console.error(e);
+    //         }
+    //         this.first = false;
+    //     });
+    // }
+
     onGenerateRandomClick(): void {
         let randomTasks = this.getIncompleteTasks(this.svcData.data);
         randomTasks = this.filterFutureChained(randomTasks);
