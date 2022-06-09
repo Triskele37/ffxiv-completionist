7e257a627cc8073f80fb13642fec78da7d9aae52
Triskele37 <triskele37@gmail.com>
2022-06-08 23:36:35 -0500

    fixed issue with decumana

--- src/app/core/service/store/migration/1.0.1/index.ts
+++ src/app/core/service/store/migration/1.0.1/index.ts
@@ -11,12 +11,12 @@ export function migrateTo_1_0_1(svcSaveStore: SaveStoreService): void {
     migrateStoryRecipes(store);
 
     store.moveTask(
-        'logs.gathering.gathering-log.logging.level.56-60',
-        'logs.gathering.gathering-log.logging.level.61-65',
+        'overall.logs.gathering.gathering-log.logging.level.56-60',
+        'overall.logs.gathering.gathering-log.logging.level.61-65',
         11
     );
 
-    store.deleteTask('duty.dutyraider-finder.trials.arr', 3);
+    store.deleteTask('overall.duty.dutyraid-finder.trials.arr', 3);
 
     store.write();
 }
