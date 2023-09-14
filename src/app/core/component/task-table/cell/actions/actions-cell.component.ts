511fb166ddcd4b539e1a97ae99d4bdfbd3310608
Triskele37 <triskele37@gmail.com>
2023-09-13 19:10:41 -0500

    removed static id column

--- src/app/core/component/task-table/cell/actions/actions-cell.component.ts
+++ src/app/core/component/task-table/cell/actions/actions-cell.component.ts
@@ -1,19 +1,27 @@
 import { Component, Input } from '@angular/core';
 
+import { Globals } from '@constant/Global';
 import { Task } from '@model/Task';
 
 @Component({
     selector: 'xiv-actions-cell',
     templateUrl: './actions-cell.component.html',
-    styleUrls: ['./actions-cell.component.scss']
+    styleUrls: ['./actions-cell.component.scss', './action.scss']
 })
 export class ActionsCellComponent {
     @Input() task: Task;
     @Input() rowIndex: number;
+
+    showCopyId: boolean = Globals.config.isAdmin;
     expanded: boolean;
 
     onCloseActions(): void {
         this.expanded = false;
+    }
+
+    copyTaskId(): void {
+        const path = this.task.fullStorageKey.replace('overall.', '');
+        navigator.clipboard.writeText(path);
     }
 
 }
--- src/app/model/DataGroup/createDataGroup/initializeColumns.ts
+++ src/app/model/DataGroup/createDataGroup/initializeColumns.ts
@@ -1,4 +1,3 @@
-import { Globals } from '@constant/Global';
 import { DataGroup } from '../';
 
 export function initializeColumns(group: DataGroup, json) {
@@ -8,15 +7,5 @@ export function initializeColumns(group: DataGroup, json) {
     }
     else if(group._parent?.columns) {
         group.columns = group._parent.columns;
-    }
-
-    // Show keys when in dev mode
-    if(Globals.config.isAdmin) {
-        if(group.columns?.[0] && group.columns[0].key !== 'fullStorageKey') {
-            group.columns.unshift({
-                key: 'fullStorageKey',
-                header: 'ID'
-            });
-        }
     }
 }
