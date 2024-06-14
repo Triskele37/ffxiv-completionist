ef31e5fcb74385f376733ff0651a4cf9d7699b69
Nicholas Simmons <triskele37@gmail.com>
2024-06-13 22:45:50 -0500

    bugfixes found testing

--- src/app/model/DataGroup/createDataGroup/fromJson.ts
+++ src/app/model/DataGroup/createDataGroup/fromJson.ts
@@ -1,9 +1,18 @@
-import { loadJson } from '@data/loader';
+import { loadJson, JSON } from '@data/loader';
 
 import { DataGroup } from '../';
 import { createDataGroup } from './';
 
 export function fromJson(parent: DataGroup, path: string): DataGroup {
     const json = loadJson(path);
+    return createDataGroup(json, parent);
+}
+
+export function fromMappedJson(
+    parent: DataGroup,
+    path: string,
+    map: (json: JSON) => JSON,
+) {
+    const json = map(loadJson(path));
     return createDataGroup(json, parent);
 }
