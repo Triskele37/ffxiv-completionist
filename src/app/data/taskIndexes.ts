ba1593a0434e2138ddb08499393fe800e0d95b6a
Nicholas Simmons <triskele37@gmail.com>
2025-02-25 14:36:21 -0600

    condensed fate links

--- src/app/data/taskIndexes.ts
+++ src/app/data/taskIndexes.ts
@@ -1,40 +1,32 @@
 import { Task } from '@model/Task';
 
-type IndexContainer = {
-    resourceBase: string;
-    shorthand: string;
-    map: Map<number, Task>;
-};
-
 /**
  * Quick lookup indexes for groups with purely unique ids for their tasks
  * - Uses Task id for the Map key
  * - Map is used to ensure uniqueness and additional perf/size gains over a raw object
  */
+type IndexContainer = {
+    baseKey: string;
+    shorthand: string;
+    map: Map<number, Task>;
+};
+
+const createIndex = (baseKey: string, shorthand: string): IndexContainer =>
+    ({ baseKey, shorthand, map: new Map<number, Task>() });
+
 const INDEX: Record<string, IndexContainer> = {
-    Achievement: {
-        resourceBase: 'character.achievement',
-        shorthand: 'a',
-        map: new Map<number, Task>(),
-    },
-    Quest: {
-        resourceBase: 'duty.quest',
-        shorthand: 'q',
-        map: new Map<number, Task>(),
-    },
-    Title: {
-        resourceBase: 'character.character.title',
-        shorthand: 't',
-        map: new Map<number, Task>(),
-    }
+    Achievement: createIndex('character.achievement', 'a'),
+    Fate: createIndex('duty.fate', 'f'),
+    Quest: createIndex('duty.quest', 'q'),
+    Title: createIndex('character.character.title', 't'),
 };
 
 /**
  * Attempt to add tasks to indexes
  */
 export function addTaskToIndex(task: Task) {
-    for(const { map, resourceBase } of Object.values(INDEX)) {
-        if(task.fullStorageKey.startsWith(resourceBase)) {
+    for(const { baseKey, map } of Object.values(INDEX)) {
+        if(task.fullStorageKey.startsWith(baseKey)) {
             map.set(task.id, task);
             break;
         }
