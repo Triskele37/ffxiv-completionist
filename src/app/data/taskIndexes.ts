f097b11693907e2678ffcc972ff2dd5f3a4a74c2
Nicholas Simmons <triskele37@gmail.com>
2025-02-25 14:21:30 -0600

    condensed title links

--- src/app/data/taskIndexes.ts
+++ src/app/data/taskIndexes.ts
@@ -1,21 +1,51 @@
 import { Task } from '@model/Task';
 
+type IndexContainer = {
+    resourceBase: string;
+    shorthand: string;
+    map: Map<number, Task>;
+};
+
 /**
  * Quick lookup indexes for groups with purely unique ids for their tasks
  * - Uses Task id for the Map key
  * - Map is used to ensure uniqueness and additional perf/size gains over a raw object
  */
-export const AchievementIndex = new Map<number, Task>();
-export const QuestIndex = new Map<number, Task>();
+const INDEX: Record<string, IndexContainer> = {
+    Achievement: {
+        resourceBase: 'character.achievement',
+        shorthand: 'a',
+        map: new Map<number, Task>(),
+    },
+    Quest: {
+        resourceBase: 'duty.quest',
+        shorthand: 'q',
+        map: new Map<number, Task>(),
+    },
+    Title: {
+        resourceBase: 'character.character.title',
+        shorthand: 't',
+        map: new Map<number, Task>(),
+    }
+};
 
+/**
+ * Attempt to add tasks to indexes
+ */
 export function addTaskToIndex(task: Task) {
-    const key = task.fullStorageKey;
-    if(key.includes('character.achievement')) AchievementIndex.set(task.id, task);
-    if(key.includes('duty.quest')) QuestIndex.set(task.id, task);
+    for(const { map, resourceBase } of Object.values(INDEX)) {
+        if(task.fullStorageKey.startsWith(resourceBase)) {
+            map.set(task.id, task);
+            break;
+        }
+    }
 }
 
 export function getIndexedTask(indexKey: string, taskId: number | string) {
     const id = typeof taskId === 'number' ? taskId : parseInt(taskId);
-    if(indexKey === 'a') return AchievementIndex[id];
-    if(indexKey === 'q') return QuestIndex[id];
+    if(isNaN(id)) return;
+
+    for(const { map, shorthand } of Object.values(INDEX)) {
+        if(indexKey === shorthand) return map.get(id);
+    }
 }
