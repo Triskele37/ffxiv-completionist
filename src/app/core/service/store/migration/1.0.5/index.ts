62b9abb8cff26d645dd2700ea6dfe4b774bc6af7
Nicholas Simmons <triskele37@gmail.com>
2025-02-25 15:50:01 -0600

    repathed dutyraid-finder

--- src/app/core/service/store/migration/1.0.5/index.ts
+++ src/app/core/service/store/migration/1.0.5/index.ts
@@ -6,6 +6,8 @@ import { migrateQuests } from './quest';
 export function migrateTo_1_0_5(svcSaveStore: SaveStoreService): void {
     const store = new ChangeStore(svcSaveStore, '1.0.5');
 
+    store.moveGroup('duty.dutyraid-finder', 'duty.duty-raid-finder');
+
     migrateQuests(store);
 
     store.write();
