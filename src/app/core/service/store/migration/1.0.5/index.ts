a819785ed334e83c63d8942ed6a49d2a45d20e04
Nicholas Simmons <triskele37@gmail.com>
2025-02-26 08:22:39 -0600

    migrated duty finder changes

--- src/app/core/service/store/migration/1.0.5/index.ts
+++ src/app/core/service/store/migration/1.0.5/index.ts
@@ -1,14 +1,14 @@
 import { SaveStoreService } from '../../save-store.service';
 import { ChangeStore } from '../ChangeStore';
 
+import { migrateDuties } from './duty';
 import { migrateQuests } from './quest';
 import { migrateLevequests } from './levequest';
 
 export function migrateTo_1_0_5(svcSaveStore: SaveStoreService): void {
     const store = new ChangeStore(svcSaveStore, '1.0.5');
 
-    store.moveGroup('duty.dutyraid-finder', 'duty.duty-raid-finder');
-
+    migrateDuties(store);
     migrateQuests(store);
     migrateLevequests(store);
 
