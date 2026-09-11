083a7e88de77b0b2554e24c92bafdf324bea4718
Nicholas Simmons <triskele37@gmail.com>
2026-09-11 02:22:15 -0500

    fixed chain-viewer not expanding links

--- desktop/renderer/app/core/component/chain-viewer/chain-viewer-node/chain-viewer-node.component.ts
+++ desktop/renderer/app/core/component/chain-viewer/chain-viewer-node/chain-viewer-node.component.ts
@@ -67,105 +67,81 @@ export class ChainViewerNodeComponent implements OnChanges {
     diveForChainGraph(task: Task): void {
         if(this.direction !== 'right') {
             const { cPrev, cPrevAny, cPrevAt, cUnlock } = task;
-            if(cPrev) this.addChildTaskBefore(cPrev, { key: cPrevAny ? 'ANY' : undefined });
+            if(cPrev) {
+                this.addChildTaskBefore(task, cPrev, { key: cPrevAny ? 'ANY' : undefined });
+            }
             if(cPrevAt) {
                 for(const k in cPrevAt) {
-                    this.addChildTaskBefore(cPrevAt[k], { at: k });
+                    this.addChildTaskBefore(task, cPrevAt[k], { at: k });
                 }
             }
 
-            if(cUnlock) this.addChildTaskBefore(cUnlock, { key: 'UNLOCK' });
+            if(cUnlock) {
+                this.addChildTaskBefore(task, cUnlock, { key: 'UNLOCK' });
+            }
         }
 
         if(this.direction === 'root') {
             const { cSiblings, cSiblingsAt, cExclude } = task;
-            if(cSiblings) this.addChildTaskAbove(cSiblings);
+            if(cSiblings) {
+                this.addChildTaskAbove(task, cSiblings);
+            }
             if(cSiblingsAt) {
                 for(const k in cSiblingsAt) {
-                    this.addChildTaskAbove(cSiblingsAt[k], { at: k });
+                    this.addChildTaskAbove(task, cSiblingsAt[k], { at: k });
                 }
             }
-            if(cExclude) this.addChildTaskBelow(cExclude);
+            if(cExclude) {
+                this.addChildTaskBelow(task, cExclude);
+            }
         }
 
         if(this.direction !== 'left') {
             const { cNext, cNextAt, cCombo, cComboAt, cUnlocks } = task;
-            if(cNext) this.addChildTaskAfter(cNext);
+            if(cNext) {
+                this.addChildTaskAfter(task, cNext);
+            }
             if(cNextAt) {
                 for(const k in cNextAt) {
-                    this.addChildTaskAfter(cNextAt[k], { at: k });
+                    this.addChildTaskAfter(task, cNextAt[k], { at: k });
                 }
             }
 
-            if(cCombo) this.addChildTaskAfter(cCombo, { key: 'COMBO' });
+            if(cCombo) {
+                this.addChildTaskAfter(task, cCombo, { key: 'COMBO' });
+            }
             if(cComboAt) {
                 for(const k in cComboAt) {
-                    this.addChildTaskAfter(cComboAt[k], {key: 'COMBO', at: k });
+                    this.addChildTaskAfter(task, cComboAt[k], { key: 'COMBO', at: k });
                 }
             }
 
-            if(cUnlocks) this.addChildTaskAfter(cUnlocks, { key: 'UNLOCK' });
-        }
-    }
-
-    addChildTaskBefore(linkOrLinks: Link | Link[], modifier?: Modifier): void {
-        const nodes = this.getLinkedNodes(linkOrLinks, true, modifier);
-        this.chainGraphPrev.push(...nodes);
-    }
-
-    addChildTaskAbove(linkOrLinks: Link | Link[], modifier?: Modifier): void {
-        const nodes = this.getLinkedNodes(linkOrLinks, false, modifier);
-        this.chainGraphSiblings.push(...nodes[0]);
-    }
-
-    addChildTaskBelow(linkOrLinks: Link | Link[], modifier?: Modifier): void {
-        const nodes = this.getLinkedNodes(linkOrLinks, false, modifier);
-        this.chainGraphExcludes.push(...nodes[0]);
-    }
-
-    addChildTaskAfter(linkOrLinks: Link | Link[], modifier?: Modifier): void {
-        const nodes = this.getLinkedNodes(linkOrLinks, false, modifier);
-        this.chainGraphNext.push(...nodes);
-    }
-
-    getLinkedNodes(linkOrLinks: Link | Link[], unshift: boolean, modifier?: Modifier): ChainNode[][] {
-        const nodeGroups: ChainNode[][] = [];
-
-        if(Array.isArray(linkOrLinks)) {
-            const nodeGroup: ChainNode[] = [];
-            linkOrLinks.forEach((link) => {
-                if(typeof link === 'string') {
-                    const content = this.svcData.get.getChild(this.svcData.data, link);
-                    if(content?.dataType === 'Task') {
-                        nodeGroup.push({ task: content, modifier });
-                    }
-                }
-                else {
-                    const task = this.task._parent.tasks.find((t) => t.id === link);
-                    if(task) nodeGroup.push({ task, modifier });
-                }
-            });
-            if(unshift) nodeGroups.unshift(nodeGroup);
-            else nodeGroups.push(nodeGroup);
-        }
-        else if(typeof linkOrLinks === 'string') {
-            const content = this.svcData.get.getChild(this.svcData.data, linkOrLinks);
-            if(content?.dataType === 'Task') {
-                const nodeGroup = [{ task: content, modifier }];
-                if(unshift) nodeGroups.unshift(nodeGroup);
-                else nodeGroups.push(nodeGroup);
+            if(cUnlocks) {
+                this.addChildTaskAfter(task, cUnlocks, { key: 'UNLOCK' });
             }
         }
-        else {
-            const task = this.task._parent.tasks.find((t) => t.id === linkOrLinks);
-            if(task) {
-                const nodeGroup = [{ task, modifier }];
-                if(unshift) nodeGroups.unshift(nodeGroup);
-                else nodeGroups.push(nodeGroup);
-            }
-        }
+    }
 
-        return nodeGroups;
+    addChildTaskBefore(task: Task, linkOrLinks: Link | Link[], modifier?: Modifier): void {
+        this.chainGraphPrev.push(this.getLinkedNodes(task, linkOrLinks, modifier));
+    }
+
+    addChildTaskAbove(task: Task, linkOrLinks: Link | Link[], modifier?: Modifier): void {
+        this.chainGraphSiblings.push(...this.getLinkedNodes(task, linkOrLinks, modifier));
+    }
+
+    addChildTaskBelow(task: Task, linkOrLinks: Link | Link[], modifier?: Modifier): void {
+        this.chainGraphExcludes.push(...this.getLinkedNodes(task, linkOrLinks, modifier));
+    }
+
+    addChildTaskAfter(task: Task, linkOrLinks: Link | Link[], modifier?: Modifier): void {
+        this.chainGraphNext.push(this.getLinkedNodes(task, linkOrLinks, modifier));
+    }
+
+    getLinkedNodes(task: Task, linkOrLinks: Link | Link[], modifier?: Modifier): ChainNode[] {
+        return this.svcData.get
+            .getTasks(linkOrLinks, task)
+            .map((chainedTask) => ({ task: chainedTask, modifier }));
     }
 
     toggleSiblingExclude(): void {
